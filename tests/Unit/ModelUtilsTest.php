32f1ad9f0a6d2acf00dbc7c594fe331063f078df
mateonunez <mateonunez@protonmail.com>
2021-08-06 22:34:06 +0200

    test(ModelUtils): add tests

---
+++ tests/Unit/ModelUtilsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Core\Utils\ModelUtils;
+use App\Core\Utils\ControllerUtils;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ModelUtilsTest extends TestCase
+{
+    /**
+     * @group model_utils
+     */
+    public function test_user_use_uuid_trait()
+    {
+        // Asserting that User class use Uuid trais
+        $this->assertTrue(ModelUtils::usesTrait(User::class, \App\Traits\Uuid::class));
+    }
+
+    /**
+     * @group model_utils
+     */
+    public function test_user_dont_use_store_validation_trait()
+    {
+        // Asserting that User does not use StoreValidation trait
+        $this->assertFalse(ModelUtils::usesTrait(User::class, \App\Traits\StoreValidation::class));
+    }
+
+    /**
+     * @group model_utils
+     */
+    public function test_user_has_no_relations()
+    {
+        $relations = ModelUtils::relations(User::class);
+
+        $this->assertEmpty($relations);
+    }
+}
